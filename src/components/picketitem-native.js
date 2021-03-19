935e833d3614f794ac03d784d1f9ec202796a3a2
parazitenew <35153121+parazitenew@users.noreply.github.com>
2021-03-19 14:41:27 +0100

    Updated deprecated import
    replaced
    `import { Picker } from '@react-native-community/picker';`
    by
    `import { Picker } from '@react-native-picker/picker';`

--- src/components/picketitem-native.js
+++ src/components/picketitem-native.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Picker } from '@react-native-community/picker';
+import { Picker } from '@react-native-picker/picker';
 
 const PickerItemNative = (props) => {
     return <Picker.Item {...props} />
